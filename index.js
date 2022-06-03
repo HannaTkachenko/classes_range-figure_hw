4acea0c16bb5eb2854a34c1209c2533f483e5b2c
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-06-03 22:54:32 +0300

    Add Figure3D
    Not working Cube

--- index.js
+++ index.js
@@ -1,50 +1,97 @@
-class RangeValidator {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
-  }
-  get from() {
-    return this._from;
-  }
-  set from(from) {
-    if (typeof from !== "number") {
-      throw new TypeError("must be number");
-    }
-    if (from <= 0 || Number.isInteger(from) === false) {
-      throw new RangeError("range includes only positive integer number");
-    }
-    this._from = from;
-  }
+// class RangeValidator {
+//   constructor(from, to) {
+//     this.from = from;
+//     this.to = to;
+//   }
+//   get from() {
+//     return this._from;
+//   }
+//   set from(from) {
+//     if (typeof from !== "number") {
+//       throw new TypeError("must be number");
+//     }
+//     if (from <= 0 || Number.isInteger(from) === false) {
+//       throw new RangeError("range includes only positive integer number");
+//     }
+//     this._from = from;
+//   }
 
-  get to() {
-    return this._to;
-  }
-  set to(to) {
-    if (this.from > to) {
-      throw new RangeError("start of range must be less than it`s end");
-    }
-    if (to <= 0 || Number.isInteger(to) === false) {
-      throw new RangeError("range includes only positive integer number");
-    }
-    if (typeof to !== "number") {
-      throw new TypeError("must be number");
-    }
-    this._to = to;
-  }
+//   get to() {
+//     return this._to;
+//   }
+//   set to(to) {
+//     if (this.from > to) {
+//       throw new RangeError("start of range must be less than it`s end");
+//     }
+//     if (to <= 0 || Number.isInteger(to) === false) {
+//       throw new RangeError("range includes only positive integer number");
+//     }
+//     if (typeof to !== "number") {
+//       throw new TypeError("must be number");
+//     }
+//     this._to = to;
+//   }
 
-  get range() {
-    return [this.from, this.to];
-  }
+//   get range() {
+//     return [this.from, this.to];
+//   }
 
-  validate(number) {
-    if (number < this.from || number > this.to) {
-      throw new Error("range NOT includes your number");
-    }
-    else if ((number === "" || number === undefined)) {
-      throw new Error("type a number, not empty string");
-    } else {
-      return `range includes ${number}`;
-    }
+//   validate(number) {
+//     if (number < this.from || number > this.to) {
+//       throw new Error("range NOT includes your number");
+//     } else if (number === "" || number === undefined) {
+//       throw new Error("type a number, not empty string");
+//     } else {
+//       return `range includes ${number}`;
+//     }
+//   }
+// }
+// const range = new RangeValidator(5, 10);
+
+class Figure3D {
+  constructor(name) {
+    this.name = name;
+  }
+  getVolume() {
+    return 0;
   }
 }
-const range = new RangeValidator(5, 10);
+
+class Sphere extends Figure3D {
+  constructor(name, radius) {
+    super("sphere");
+    this.radius = radius;
+  }
+
+  getVolume() {
+    return (4 / 3) * Math.PI * (this.radius * this.radius * this.radius);
+  }
+}
+
+class Cylinder extends Sphere {
+  constructor(name, radius, height) {
+    super("cylinder", radius);
+    this.height = height;
+  }
+  getVolume() {
+    return Math.PI * (this.radius * this.radius) * this.height;
+  }
+}
+
+class Cube extends Cylinder {
+  constructor(name, height) {
+    super("cube", height);
+  }
+
+  getVolume() {
+    return this.height * this.height * this.height;
+  }
+}
+
+const sphere = new Sphere("sphere", 10);
+const cylinder = new Cylinder("cylinder", 10, 10);
+const cube = new Cube("cube", 10);
+
+console.log(sphere.getVolume(10));
+console.log(cylinder.getVolume(10, 10));
+console.log(cube.getVolume(10));
