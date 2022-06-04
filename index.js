357876313b1ee093257ecfd9a8da5da4abc2a974
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-06-04 20:17:28 +0300

    Fix Cube

--- index.js
+++ index.js
@@ -1,52 +1,63 @@
-// class RangeValidator {
-//   constructor(from, to) {
-//     this.from = from;
-//     this.to = to;
-//   }
-//   get from() {
-//     return this._from;
-//   }
-//   set from(from) {
-//     if (typeof from !== "number") {
-//       throw new TypeError("must be number");
-//     }
-//     if (from <= 0 || Number.isInteger(from) === false) {
-//       throw new RangeError("range includes only positive integer number");
-//     }
-//     this._from = from;
-//   }
+//Создать класс RangeValidator, со следующими свойствами:
+//■ from (number);
+//■ to (number);
+//Реализовать getter'ы и setter'ы для обоих свойств
+//Реализовать getter range, который будет возвращать массив с двумя числами диапазона
+//Реализовать метод validate, который будет принимать число и проверить входит ли число в указанный диапазон. Метод возвращает значение, если значение валидно. И кинет ошибку, если нет.
 
-//   get to() {
-//     return this._to;
-//   }
-//   set to(to) {
-//     if (this.from > to) {
-//       throw new RangeError("start of range must be less than it`s end");
-//     }
-//     if (to <= 0 || Number.isInteger(to) === false) {
-//       throw new RangeError("range includes only positive integer number");
-//     }
-//     if (typeof to !== "number") {
-//       throw new TypeError("must be number");
-//     }
-//     this._to = to;
-//   }
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  get from() {
+    return this._from;
+  }
+  set from(from) {
+    if (typeof from !== "number") {
+      throw new TypeError("must be number");
+    }
+    if (from <= 0 || Number.isInteger(from) === false) {
+      throw new RangeError("range includes only positive integer number");
+    }
+    this._from = from;
+  }
 
-//   get range() {
-//     return [this.from, this.to];
-//   }
+  get to() {
+    return this._to;
+  }
+  set to(to) {
+    if (this.from > to) {
+      throw new RangeError("start of range must be less than it`s end");
+    }
+    if (to <= 0 || Number.isInteger(to) === false) {
+      throw new RangeError("range includes only positive integer number");
+    }
+    if (typeof to !== "number") {
+      throw new TypeError("must be number");
+    }
+    this._to = to;
+  }
 
-//   validate(number) {
-//     if (number < this.from || number > this.to) {
-//       throw new Error("range NOT includes your number");
-//     } else if (number === "" || number === undefined) {
-//       throw new Error("type a number, not empty string");
-//     } else {
-//       return `range includes ${number}`;
-//     }
-//   }
-// }
-// const range = new RangeValidator(5, 10);
+  get range() {
+    return [this.from, this.to];
+  }
+
+  validate(number) {
+    if (number < this.from || number > this.to) {
+      throw new Error("range NOT includes your number");
+    } else if (number === "" || number === undefined) {
+      throw new Error("type a number, not empty string");
+    } else {
+      return `range includes ${number}`;
+    }
+  }
+}
+const range = new RangeValidator(5, 10);
+
+//Создать класс Figure3D
+//Создать классы-потомки для Шара, Цилиндра и Куба.
+//У всех классов должен быть метод для рассчета объема.
 
 class Figure3D {
   constructor(name) {
@@ -78,9 +89,10 @@ class Cylinder extends Sphere {
   }
 }
 
-class Cube extends Cylinder {
+class Cube extends Figure3D {
   constructor(name, height) {
-    super("cube", height);
+    super("cube");
+    this.height = height;
   }
 
   getVolume() {
